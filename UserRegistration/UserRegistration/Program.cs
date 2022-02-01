5eb6f252117be989ce0fdfd7cea4da8ae954ff18
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-02-01 14:20:07 +0530

    [Vinayak] adding valid first name

---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using UserRegistration;
+
+Console.WriteLine("Hello, World!");
+ValidUserRegistraton validUserRegistraton = new ValidUserRegistraton();
+Console.WriteLine("Enter valid first name");
+string frstname = Console.ReadLine();
+validUserRegistraton.ValidateFName(frstname);
---
+++ UserRegistration/UserRegistration/ValidUserRegistraton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class ValidUserRegistraton
+    {
+        //declaring pattern
+        public const string FIRST_NAME = "^[A-Z][a-zA-Z]{2}";
+
+        public void ValidateFName(string fName)
+        {
+            //assigning pattern in regex constructor
+            Regex regex = new Regex(FIRST_NAME);
+
+            if (regex.IsMatch(fName))
+            {
+                Console.WriteLine("first name is matched with pattern");
+            }
+            else
+            {
+                Console.WriteLine("first name does not match with pattern");
+            }
+        }
+    }
+}
